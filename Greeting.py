edd8b939b75e1f68dacad7042facea54f3aa6249
R.Ito <kawboy442@gmail.com>
2020-08-17 11:17:19 +0900

    feat: 多重配列にしてみる

--- Greeting.py
+++ Greeting.py
@@ -1,7 +1,6 @@
-a = ["sato", "suzuki", "takahashi"]
+a = [["sato", "suzuki"], ["takahashi", "tanaka"]]
 
-a[1] = "tanaka"
-
-print(a[0])
-print(a[1])
-print(a[2])
+print(a[0][0])
+print(a[0][1])
+print(a[1][0])
+print(a[1][1])
